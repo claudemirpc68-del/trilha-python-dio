4f00aaa233c188f62b2ac89d768902d6188208a6
claudemir p cubas <claudemirpc68@gmail.com>
2025-09-09 18:35:13 -0300

    Update convertendo_tipos.py
    ACRESCENTEI NOVOS EXEMPOLS

--- 00 - Fundamentos/convertendo_tipos.py
+++ 00 - Fundamentos/convertendo_tipos.py
@@ -1,3 +1,21 @@
+print(int(1.97348728))  # Converte float para int (descarta decimais)
+print(int("10"))        # Converte string para int
+
+print(float("10.10"))   # Converte string para float
+print(float(100))       # Converte int para float
+
+valor = 10
+valor_str = str(valor)  # Converte int para string
+print(type(valor))      # Mostra tipo original
+print(type(valor_str))  # Mostra tipo convertido
+
+print(100 / 2)          # Divisão comum (float)
+print(100 // 2)         # Divisão inteira (int)
+
+
+
+
+
 print(int(1.97348728))
 print(int("10"))
 print(float("10.10"))
